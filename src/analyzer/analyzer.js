9148e789a27d8347c95cded9968be81e95085b31
xiekun1992 <xkfront@outlook.com>
2018-05-16 01:34:30 +0800

    添加分析器日志记录

--- src/analyzer/analyzer.js
+++ src/analyzer/analyzer.js
@@ -1,20 +1,23 @@
 const { spawn } = require('child_process');
 const querystring = require('querystring');
+const logger = require('../utils/logger');
 
 function analyze () {
     return new Promise((resolve, reject) => {
         const mu = spawn('D:\\Program Files (x86)\\MuMu\\emulator\\nemu\\EmulatorShell\\NemuPlayer.exe', []);
         mu.stderr.on('data', data => {
-            reject(`nemu player error due to: ${data}`);
+            console.log(`nemu player error due to: ${data}`);
+            logger.log(`nemu player error due to: ${data}`);
         });
         mu.on('close', code => {
             console.log(`nemu player exited with code ${code}`);
+            logger.log(`nemu player exited with code ${code}`);
         });
         
         const ws = spawn('D:\\Program Files\\Wireshark\\tshark.exe', ['-i', 'WLAN', '-w', '-', 'port 80 and tcp[((tcp[12:1] & 0xf0) >> 2):4] = 0x47455420']);
         ws.stdout.on('data', data => {
           data = data.toString();
-          console.log(data)
+          // console.log(data)
           if (/\/app\/actcenter\/index\/speed\/1/g.test(data)) {
             ws.kill();
             mu.kill();
@@ -29,10 +32,12 @@ function analyze () {
         });
         ws.stderr.on('data', data => {
           console.log(`wireshark error due to: ${data}`);
+          logger.log(`wireshark error due to: ${data}`);
         });
         ws.on('close', code => {
           mu.kill();
           console.log(`wireshark exited with code ${code}`);
+          logger.log(`wireshark exited with code ${code}`);
         });
     });
 }
