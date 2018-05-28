5356a08131553e1d59501e8956842c27d1f9a95d
xiekun1992 <xkfront@outlook.com>
2018-05-28 22:05:39 +0800

    修改时间格式化后长度不一致的问题

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
 Date.prototype.format = function formatDate() {
   const date = this;
-  return `${date.getFullYear()}-${(date.getMonth() + 1 + '').padStart(2, 0)}-${(date.getDate() + '').padStart(2, 0)} ${(date.getHours() + '').padStart(2, 0)}'${(date.getMinutes() + '').padStart(2, 0)}'${(date.getSeconds() + '').padStart(2, 0)}.${date.getMilliseconds()}`;
+  return `${date.getFullYear()}-${(date.getMonth() + 1 + '').padStart(2, 0)}-${(date.getDate() + '').padStart(2, 0)} ${(date.getHours() + '').padStart(2, 0)}'${(date.getMinutes() + '').padStart(2, 0)}'${(date.getSeconds() + '').padStart(2, 0)}.${(date.getMilliseconds() + '').padStart(3, 0)}`;
 };
